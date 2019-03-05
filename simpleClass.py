2b993572ffcf98a1e3744579a33b1185716da625
odhran456 <44904580+odhran456@users.noreply.github.com>
2019-03-05 20:38:15 +0000

    Create simpleClass.py

---
+++ simpleClass.py
@@ -0,0 +1,14 @@
+def summation(a,b):
+    return a + b
+
+print(summation(4,7))
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def myfunc(self):
+        print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
